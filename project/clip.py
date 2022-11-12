5ca94b105a1035d9793904743960d116eab64e35
shintaro0930 <ozaki.shintaro@icloud.com>
2022-11-12 23:14:11 +0900

    fixed

--- project/clip.py
+++ project/clip.py
@@ -6,25 +6,20 @@ import sys
 sys.path.append('CLIP')
 import torch
 from CLIP import clip
-import matplotlib.pyplot as plt
 from PIL import Image
+# PILの説明: https://note.nkmk.me/python-pillow-basic/
 from googletrans import Translator
  
 device = "cuda" if torch.cuda.is_available() else "cpu"
 model, preprocess = clip.load("ViT-B/32", device=device)
 
-# これは個別にインストールする必要がある
-# !pip install googletrans==4.0.0-rc1
-# from googletrans import Translator
-
 file_base_dir = '/work/pictures/'
-# file_base_dir = '/work/project/Monster/'
-# texts_jp = ["スライムに乗った騎士", "馬に乗った騎士", "剣で遊んでいる騎士", "スライム", "スライムに乗った魔法使い"]
 texts_jp = ["車が写っている晴天の日", "大きな建物", "木とガラスが写った美術館", "様々な色のガラス"]
-# monsters = ['Dragon-knight']
 monsters = ['IMG_8727', 'IMG_9576', 'IMG_9603', 'IMG_9615', 'IMG_9649', 'IMG_9657']
 
+# 多言語に翻訳
 translator = Translator()
+# dest:翻訳先の言語(destination), src:翻訳元の言語(source)
 texts_en = [translator.translate(text_jp, dest="en", src="ja").text for text_jp in texts_jp]
 # print(texts_en)
 
@@ -32,18 +27,18 @@ for i, monster in enumerate(monsters):
   try:
     original_image = Image.open(file_base_dir+f"{monster}.png")
     print(f'--- {monsters[i]} ---')
+    #画像の前処理
     image = preprocess(original_image).unsqueeze(0).to(device)
-    plt.figure()
-    plt.imshow(original_image)
-    plt.axis('off')
-    plt.show()
-  
+
+    #tokenize
     text = clip.tokenize(texts_en).to(device)
   
     with torch.no_grad():
+        #エンコード
         image_features = model.encode_image(image)
         text_features = model.encode_text(text)
         
+        #推論
         logits_per_image, logits_per_text = model(image, text)
         probs = logits_per_image.softmax(dim=-1).cpu().numpy()
 
@@ -54,7 +49,3 @@ for i, monster in enumerate(monsters):
     # 例外処理        
   except Exception as e:
     continue
-
-    # # 結果表示 
-    # for i in range(probs.shape[-1]):
-    #     print(f'{texts_jp[i]}({texts_en[i]}): {probs[0, i]*100:0.1f}%')
