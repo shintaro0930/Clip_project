bcd2a25ca595deafffe9749037ca6018d65ce86c
shintaro0930 <ozaki.shintaro@icloud.com>
2022-11-15 17:53:55 +0900

    add random generator

---
+++ project/random_gen.py
@@ -0,0 +1,22 @@
+import random
+import os
+from googletrans import Translator
+
+# https://wak-tech.com/archives/1625
+translator = Translator()
+
+# import adjectives list
+with open("EnglishList/english-adjectives.txt") as f:
+    adjectives = f.readlines()
+    
+# import nouns list
+with open("EnglishList/english-nouns.txt") as f:
+    nouns = f.readlines()
+
+with open('./texts/rand_text.txt', 'a') as f:
+    for i in range(10):
+        adj_index = random.randrange(len(adjectives))
+        noun_index = random.randrange(len(nouns))
+        theme = adjectives[adj_index]+' '+nouns[noun_index]
+        theme = theme.replace("\n", '')
+        f.writelines(translator.translate(theme, dest="ja", src="en").text + '\n')
