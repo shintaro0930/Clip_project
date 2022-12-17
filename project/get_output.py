21a48eef1f1f4e668d64f21628989d7c6e75cfda
shintaro0930 <ozaki.shintaro@icloud.com>
2022-12-17 11:52:12 +0900

    update get_output.py file

--- project/get_output.py
+++ project/get_output.py
@@ -1,6 +1,27 @@
 import re
+import numpy as np
+from sklearn.metrics.pairwise import cosine_similarity
+from janome.tokenizer import Tokenizer
+from sklearn.feature_extraction.text import TfidfVectorizer
 
+# change text to the style of wakachi
+def wakachi(text)->list:
+    t = Tokenizer()
+    tokens = t.tokenize(text)
+    docs = []
+    for token in tokens:
+        docs.append(token.surface)
+    return docs
+
+def vecs_array(documents):
+    docs = np.array(documents)
+    vectorizer = TfidfVectorizer(analyzer=wakachi,binary=True,use_idf=False)
+    vecs = vectorizer.fit_transform(docs)
+    return vecs.toarray()
+
+# ========================================
 # input_text = input("input:")
+input_text = "赤い神社"
 
 with open('output.txt') as f:
     lines:list = f.readlines()
@@ -8,25 +29,46 @@ with open('output.txt') as f:
 line_list = []
 prob_list = []
 line_list_list = []
+prob_list_list = []
 count = 0
 for line in lines:
-    count += 1
     if(re.search('[=]+', line)):
         continue
+    count += 1
     line = line.rstrip()
     prob = line
     line = line.split(':')[0]
     line_list.append(line)
     prob = prob.split(' ')[-1]
     prob_list.append(prob)
-    print(f'str: {line}')
-    print(f'prob: {prob}')
-    if(count / 3 == 0):
+    if(count % 4 == 0):
         line_list_list.append(line_list)
+        prob_list_list.append(prob_list)
+        line_list = []
+        prob_list = []
 
-for out in line_list_list:
-    print("入ってない")
-    print(out)
 
+max_prob = 0
+save_images = []
+#print(line_list_list)
+for (line_list,prob_list) in zip(line_list_list, prob_list_list):
+    image_name = line_list.pop(0)
+    line_list.append(input_text)
+    cos_sim = np.round(cosine_similarity(vecs_array(line_list), vecs_array(line_list)), len(line_list))
+    cos_list = cos_sim[-1].tolist()           # .tolist()で numpy.ndarray --> list
+    cos_list.pop(-1)
+    avg = sum(cos_list)/ len(cos_list) * 100      # %表示
+    print(f'{image_name}と{input_text}の類似度: {avg}%')
+    if(avg > max_prob):
+        save_images.clear()
+        save_images.append(image_name)
+        max_prob = avg
+    elif (avg == max_prob):
+        save_images.append(image_name)
 
-print("hello")
+if(max_prob == 0):
+    print('入力文に合う画像はありません')
+else:
+    print(f'入力文に合う確率は{max_prob:0.2f}%です。その画像は\n')
+    for image in save_images:
+        print(image)
